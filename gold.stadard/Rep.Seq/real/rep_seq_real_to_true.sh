b335888c647cbaeacbfbe1129b9471df8a1cd61b
Keith <keithgeraldmitchell@gmail.com>
2018-05-02 17:58:48 -0700

    blah

--- gold.stadard/Rep.Seq/real/rep_seq_real_to_true.sh
+++ gold.stadard/Rep.Seq/real/rep_seq_real_to_true.sh
@@ -3,5 +3,5 @@
 
 chmod +x rep_seq_real_to_true.py
 
-python "/u/home/k/keithgmi/project-zarlab/error.correction.benchmarking/gold.stadard/Rep.Seq/real/" "/u/flashscratch/k/keithgmi/rep_seq_rea_true/" "/u/home/s/serghei/project/EC_survey/RepSeq_real/"
+python "/u/home/k/keithgmi/project-zarlab/error.correction.benchmarking/gold.stadard/Rep.Seq/real/rep_seq_real_to_true.py" "/u/flashscratch/k/keithgmi/rep_seq_rea_true/" "/u/home/s/serghei/project/EC_survey/RepSeq_real/"
 
