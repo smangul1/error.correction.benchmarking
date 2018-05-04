b01750319f009c9191d46377b277e990dc6f0d85
Keith <keithgeraldmitchell@gmail.com>
2018-05-04 16:56:46 -0700

    shell cleanup

--- gold.stadard/Rep.Seq/real/run.sh
+++ gold.stadard/Rep.Seq/real/run.sh
@@ -1,14 +1,3 @@
 . /u/local/Modules/default/init/modules.sh
 module load python
-
-# testing environment
-#TRUE_DIR="/media/sf_EC3/gold.stadard/Rep.Seq/real/true"
-#RAW_DIR="/media/sf_EC3/gold.stadard/Rep.Seq/real/data"
-#
-# hoffman environment
-TRUE_DIR = "/u/flashscratch/k/keithgmi/rep_seq_real_true/"
-RAW_DIR = "/u/home/s/serghei/project/EC_survey/RepSeq_real/"
-#
-
-# THIS IS FOR REAL
-python rep_seq_real_to_true.py -output_dir "$TRUE_DIR" -input_dir "$RAW_DIR"
+python rep_seq_real_to_true.py -output_dir "/u/flashscratch/k/keithgmi/rep_seq_real_true/" -input_dir "/u/home/s/serghei/project/EC_survey/RepSeq_real/"
