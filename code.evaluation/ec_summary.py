dcb02ac42bac5b391900a9d44532ff000e32305a
Keith <keithgeraldmitchell@gmail.com>
2018-05-22 13:31:17 -0700

    updates to ec summary for recall, precision, and gain for base and read level

--- code.evaluation/ec_summary.py
+++ code.evaluation/ec_summary.py
@@ -8,6 +8,10 @@
 #  Functions Contained: check_existence, append_summary
 #####################################################################################
 
+from __future__ import division
+import argparse
+import csv
+import os
 
 def check_existence(out_dir, file_name):
     if os.path.exists(out_dir + file_name):
@@ -17,41 +21,52 @@ def check_existence(out_dir, file_name):
 
 
 def append_summary(wrapper, kmer, read_data, base_data, ec_name, outdir, filename):
-    read_sum = {"TN": 0, "TP": 0, "FN": 0, "FP(NORMAL)": 0, "FP(INDEL)": 0, "FP(TRIMMING)": 0, "Read Total": 0}
+    read_sum = {"TN": 0, "TP": 0, "FN": 0, "FP(NORMAL)": 0, "FP(INDEL)": 0, "FP(TRIMMING)": 0, "Read Total": 0, "Not TN": 0}
     base_sum = {"Total Bases": 0, "TP": 0, "FP(NORMAL)": 0, "FN": 0, "TN": 0, "FP(INDEL)": 0, "FP(TRIMMING)": 0}
-    with open(read_data, 'r') as reads_csv, open(base_data, 'r') as base_csv:
+    with open(read_data, 'r') as reads_csv:
+        with open(base_data, 'r') as base_csv:
+            reads_reader = csv.reader(reads_csv, delimiter=',')
+            for row in reads_reader:
+                read_sum[str(row[1])] += 1
+                read_sum["Not TN"] += 1
 
-        reads_reader = csv.reader(reads_csv, delimiter=',')
-        for row in reads_reader:
-            read_sum[str(row[1])] += 1
+            base_reader = csv.reader(base_csv, delimiter=',')
+            for row in base_reader:
+                # NOTE: we take the "Read Total" here since we dont record TN reads but we record everything for
+                # base evaluation so we know this is the number of reads that were analyzed
+                read_sum["Read Total"] += 1
 
-        base_reader = csv.reader(base_csv, delimiter=',')
-        for row in base_reader:
-            # NOTE: we take the "Read Total" here since we dont record TN reads but we record everything for
-            # base evaluation so we know this is the number of reads that were analyzed
-            read_sum["Read Total"] += 1
+                # Aggregrate the base data per file
+                base_sum["Total Bases"] += int(row[1])
+                base_sum["TP"] += int(row[2])
+                base_sum["FP(NORMAL)"] += int(row[3])
+                base_sum["FN"] += int(row[4])
+                base_sum["TN"] += int(row[1])-(int(row[2]) + int(row[3]) + int(row[4]))
+                base_sum["FP(INDEL)"] += int(row[5])
+                base_sum["FP(TRIMMING)"] += int(row[6])
 
-            # Aggregrate the base data per file
-            base_sum["Total Bases"] += row[1]
-            base_sum["TP"] += row[2]
-            base_sum["FP(NORMAL)"] += row[3]
-            base_sum["FN"] += row[4]
-            base_sum["TN"] += row[1]-(row[2]+row[3]+row[4])
-            base_sum["FP(INDEL)"] += row[5]
-            base_sum["FP(TRIMMING)"] += row[6]
+            read_sum["TN"] = read_sum["Read Total"] - read_sum["Not TN"]
 
-    
     type = check_existence(outdir, filename)
-    with open(base_dir_join + file_name, type) as summaryout:
+    with open(outdir + filename, type) as summaryout:
         summaryout = csv.writer(summaryout, delimiter=',')
-        if type == 'w':
-            summaryout.writerow("EC Filename", "Wrapper Name", "Kmer Size",
-                                "Read - TP", "Read - TN", "Read - FN", "Read - FP(NORMAL)", "Read - FP(TRIMMING)", "Read - FP (INDEL)", "Total Reads",
-                                "Base - TP", "Base - TN", "Base - FN", "Base - FP(NORMAL)",	"Base - FP(TRIMMING)", "Base - FP (INDEL)", "Total Bases")
 
-        summaryout.writerow(ec_name, wrapper, kmer,
-                            read_sum["TP"], read_sum["TN"], read_sum["FN"], read_sum["FP(NORMAL)"], read_sum["FP(TRIMMING)"], read_sum["FP(INDEL)"], read_sum["Read Total"],
-                            base_sum["TP"], base_sum["TN"], base_sum["FN"], base_sum["FP(NORMAL)"], base_sum["FP(TRIMMING)"], base_sum["FP(INDEL)"], base_sum["Total Bases"])
+        if type == 'w':
+            write_header=["EC Filename", "Wrapper Name", "Kmer Size",
+                            "Read - TP", "Read - TN", "Read - FN", "Read - FP(NORMAL)", "Read - FP(TRIMMING)", "Read - FP (INDEL)", "Total Reads",
+                            "Base - TP", "Base - TN", "Base - FN", "Base - FP(NORMAL)",	"Base - FP(TRIMMING)", "Base - FP (INDEL)", "Total Bases",
+                            "Read - Recall", "Read - Precision", "Read - Gain",
+                            "Base - Recall", "Base - Precision", "Base - Gain"]
+            summaryout.writerow(write_header)
+
+        total_read_FP = read_sum["FP(TRIMMING)"] + read_sum["FP(INDEL)"] + read_sum["FP(NORMAL)"]
+        total_base_FP = base_sum["FP(TRIMMING)"] + base_sum["FP(INDEL)"] + base_sum["FP(NORMAL)"]
+        write_data=[ec_name, wrapper, kmer,
+                    read_sum["TP"], read_sum["TN"], read_sum["FN"], read_sum["FP(NORMAL)"], read_sum["FP(TRIMMING)"], read_sum["FP(INDEL)"], read_sum["Read Total"],
+                    base_sum["TP"], base_sum["TN"], base_sum["FN"], base_sum["FP(NORMAL)"], base_sum["FP(TRIMMING)"], base_sum["FP(INDEL)"], base_sum["Total Bases"],
+                    float(read_sum["TP"]/(read_sum["TP"]+read_sum["FN"])),float(read_sum["TP"]/(read_sum["TP"] + total_read_FP)),float((read_sum["TP"]-total_read_FP)/(read_sum["TP"] + read_sum["FN"])),
+                    float(base_sum["TP"]/(base_sum["TP"]+base_sum["FN"])),float(base_sum["TP"]/(base_sum["TP"] + total_base_FP)),float((base_sum["TP"]-total_base_FP)/(base_sum["TP"] + base_sum["FN"]))]
+        summaryout.writerow(write_data)
 
 
 
@@ -75,7 +90,7 @@ if __name__ == "__main__":
     read_data = args['read_data']
     base_data = args['base_data']
     ec_name = args['ec_name']
-    out_dir = args['out_dir']
+    out_dir = args['outdir']
 
     filename='master_summary.txt'
     append_summary(wrapper, kmer, read_data, base_data, ec_name, out_dir, filename)
